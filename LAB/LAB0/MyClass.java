4417712988bdcb9557c176ad32abfc1e296fab75
Hygel1 <75685854+Hygel1@users.noreply.github.com>
2024-08-27 17:40:07 -0400

    up

---
+++ LAB/LAB0/MyClass.java
@@ -0,0 +1,19 @@
+
+/**
+ * Uses main method to print "Hello World" to console
+ *
+ * @author Sean McLoughlin
+ * @version 1.0 8/27/24
+ */
+public class MyClass
+{
+    /**
+     * Main method - prints "Hello World"
+     * @param args array of String values to be printed
+     */
+    public static void main(String[] args){
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
